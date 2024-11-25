c702b8f794430745e1dd41423f7e43cddfc3d516
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-11-25 22:53:59 +0000

    Function to process data from api

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import "./styles.css";
 
 async function getWeatherData(location, unitGroup) {
   const weatherData = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&elements=datetime%2Ctemp%2Cfeelslike%2Chumidity%2Cwindspeedmean%2Cconditions&include=current&key=J34LGPNZGQ95XYK8YUGDAAGSY&contentType=json`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&elements=datetime%2Ctemp%2Cfeelslike%2Chumidity%2Cconditions&include=current&key=J34LGPNZGQ95XYK8YUGDAAGSY&contentType=json`,
     { mode: "cors" },
   );
 
   if (weatherData.status == 200) {
     const jsonData = await weatherData.json();
-    console.log(jsonData);
     return jsonData;
   } else if (weatherData.status == 400) {
     throw `"${location}" is not a valid location`;
@@ -17,6 +16,18 @@ async function getWeatherData(location, unitGroup) {
   throw new Error(weatherData.status);
 }
 
-getWeatherData("london", "uk").catch((error) => {
-  console.log(error);
-});
+function processWeatherData(data) {
+  return {
+    location: data.resolvedAddress,
+    temperatue: data.currentConditions.temp,
+    "feels like": data.currentConditions.feelslike,
+    humidity: data.currentConditions.humidity,
+    conditions: data.currentConditions.conditions,
+  };
+}
+
+getWeatherData("london", "uk")
+  .catch((error) => {
+    console.log(error);
+  })
+  .then((data) => console.log(processWeatherData(data)));
